e2f38ea022ad6a897fdd5cb57763efa445c3294d
Anders Lindhardt Madsen <andem20@student.sdu.dk>
2021-03-26 12:19:50 +0100

    Created game of life

--- wasm-test/site/index.js
+++ wasm-test/site/index.js
@@ -1,4 +1,10 @@
 import("./node_modules/wasm-test/wasm_test.js").then((js) => {
     let result = js.greet("Anders");
     console.log(result);
+
+    let array1 = new Float32Array([1,2,3,4]);
+    let array2 = new Float32Array([1,20,3,4]);
+
+    let compareArrays = js.compare_arrays(array1, array2);
+    console.log(compareArrays);
 });
--- wasm-test/src/lib.rs
+++ wasm-test/src/lib.rs
@@ -4,3 +4,14 @@ use wasm_bindgen::prelude::*;
 pub fn greet(name: &str) -> String {
     String::from(format!("Hello {}", name))
 }
+
+#[wasm_bindgen]
+pub fn compare_arrays(array1: &[f32], array2: &[f32]) -> bool {
+    if array1.len() != array2.len() { return false; }
+
+    for i in 0..array1.len() {
+        if array1[i] != array2[i] { return false; }
+    }
+
+    return true;
+}
