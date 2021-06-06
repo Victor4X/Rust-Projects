7951b61ef7290d1d08d9de2d5b258c865d249866
Victor <viggo00@hotmail.com>
2021-06-06 22:42:00 +0200

    Initial argument handling

--- cli/grrs/src/main.rs
+++ cli/grrs/src/main.rs
@@ -1,3 +1,19 @@
+use structopt::StructOpt;
+
+// Below is documentation <3 rust <3
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    let path_str: String = args.path.into_os_string().into_string().unwrap();
+    println!("Pattern: {}, Path: {}", args.pattern, path_str);
 }
