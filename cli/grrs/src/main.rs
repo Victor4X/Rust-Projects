c92d263d1d6d0ce1947ec1a4fe7b0f5c44570caa
Victor <viggo00@hotmail.com>
2021-06-06 23:16:19 +0200

    started nicer errors

--- cli/grrs/src/main.rs
+++ cli/grrs/src/main.rs
@@ -14,6 +14,22 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
 
-    let path_str: String = args.path.into_os_string().into_string().unwrap();
-    println!("Pattern: {}, Path: {}", args.pattern, path_str);
+    // Print arguments
+    println!("Pattern: {}, Path: {}", args.pattern, args.path.as_path().to_str().unwrap());
+
+    // Read file
+    let result = std::fs::read_to_string("test.txt");
+    let content = match result {
+        Ok(content) => { content },
+        Err(error) => { panic!("Can't deal with {}, just exit here", error); }
+    };
+    println!("file content: {}", content);
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+
 }
